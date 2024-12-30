360858c8f404980c010e9166b00b884e4a9a3806
Gray <silicongray@gmail.com>
2024-12-30 10:42:31 +0500

    Improve home tab, design & implement booking button on home tab

--- src/home.js
+++ src/home.js
@@ -9,18 +9,31 @@ class Home {
     console.log('at home');
     let content = document.getElementById('content');
     let h1Ele = document.createElement('h1');
-    h1Ele.textContent = 'An Experience To Remember...';
+    h1Ele.textContent = 'A Surreal Experience...';
     content.appendChild(h1Ele);
 
-    let pEle = document.createElement('p');
-    pEle.textContent = `Dining out is never about just eating. Its about the experience. Otherwise
+    let p1Ele = document.createElement('p');
+    p1Ele.textContent = `"The fondest memories are made when gathered around the table." - Unknown`;
+    p1Ele.setAttribute('id', 'p1Ele');
+    content.appendChild(p1Ele);
+
+    let p2Ele = document.createElement('p');
+    p2Ele.textContent = `Dining out is never about just eating. Its about the experience. Otherwise
      you could have just ordered, Right? We provide you with an experience you'd remember for days
       & long for it again. Eating in a natural cozy environment with birds chirping around & no 
       traffic , no crowds around till miles. Just you & something special you ordered.`;
-    content.appendChild(pEle);
+    content.appendChild(p2Ele);
     // <div id="images">
     //   <img src="./images/1.jpg" alt="">
     // </div>
+
+
+
+    let homeBookingBtn = document.createElement('button');
+    homeBookingBtn.setAttribute('id', 'home-booking-btn');
+    homeBookingBtn.textContent = 'Book now!';
+    content.appendChild(homeBookingBtn);
+
     let imgDiv = document.createElement('div');
     imgDiv.setAttribute('id', 'images-div');
     content.appendChild(imgDiv);
@@ -32,7 +45,16 @@ class Home {
     img.setAttribute('alt', '');
     imgDiv.appendChild(img);
     }
+
+
   }
+  // redirectToMenu() {
+  //   let bookingButton = document.getElementById('home-booking-btn');
+  //   bookingButton.addEventListener('click', (e) => {
+  //     this.clearContent();
+  //     this.createMenuContent();
+  //   });
+  // }
 }
 
 export const { createHomeContent } = new Home();
--- src/index.js
+++ src/index.js
@@ -6,52 +6,97 @@ import { createMenuContent, Menu } from "./menu.js"
 import { About } from "./about.js"
 
 class Controller {
-  // setting up objects for different modules:
+
   constructor() {
-    // this.homeObj = home.Home
+
   }
 
   start() {
     // this.setObjects();
-    this.setUpTabListeners();
+
+    this.applyCurrentTabStyle();
+    createHomeContent();
+    this.setUpEventListeners();
   }
 
-  // setObjects() {
-  //   // this.homeObj = new Home();
-  //   // this.menuObj = new Menu();
-  //   // this.aboutObj = new About();
-  // }
   // setting up event listeners for tab switching logic:
-  setUpTabListeners() {
+  setUpEventListeners() {
+    
     let tabsArr = document.querySelectorAll('.tab-btn');
     tabsArr.forEach((tabBtn) => {
       tabBtn.addEventListener('click', (e) => {
         console.log('in index.js listener...');
+        this.applyCurrentTabStyle(e);
         this.clearContent();
         // this.homeObj.createHomeContent();
         switch (e.target.id) {
-          case 'home-btn':
-            // alert('home-btn');
+          case 'homeTab-btn':
             createHomeContent();
+            // As content will be cleared on each tab change, so have to set
+            //  booking button listener every time:
+            this.setUpBookingBtnListener();
             break;
-          case 'menu-btn':
+          case 'menuTab-btn':
             // alert('menu-btn');
             createMenuContent();
             break;
-          case 'about-btn':
+          case 'aboutTab-btn':
             // alert('about-btn');
             break;
-        }
+        };
+       
+      });
+    });
+    // set it up when page loads first time:
+    this.setUpBookingBtnListener();
+  };
 
-        // console.log(object);
-      })
-    })
+  applyCurrentTabStyle(e = '') {
+    let currEleId;
+    if (e == '') {
+      currEleId = 'homeTab-btn';
+    }
+    else {
+      currEleId = e.target.id;
+    }
+
+    if (currEleId == 'home-booking-btn') {
+
+    }
+
+    let tabBtnsArr = document.querySelectorAll('.tab-btn');
+    for (let i = 0; i < tabBtnsArr.length; i++) {
+      // for styling tab buttons:
+      if (tabBtnsArr[i].id == currEleId) {
+        tabBtnsArr[i].style.background = '#add8e6';
+        // tabBtnsArr[i].style.color = '#4682b4';
+      }
+      // for styling menu button:
+      else if (currEleId == 'home-booking-btn') {
+        tabBtnsArr[1].style.background = '#add8e6';
+        tabBtnsArr[0].style.background = '';
+        tabBtnsArr[2].style.color = '#69717a';
+      }
+      else {
+        tabBtnsArr[i].style.background = '';
+        tabBtnsArr[i].style.color = '#69717a';
+      }
+    }
+
   }
 
   clearContent() {
     document.getElementById('content').textContent = '';
   }
 
+  setUpBookingBtnListener() {
+    let bookingBtnEle = document.getElementById('home-booking-btn');
+    bookingBtnEle.addEventListener('click', e => {
+      this.clearContent();
+      this.applyCurrentTabStyle(e);
+      createMenuContent();
+    });
+  }
 
 }
 
